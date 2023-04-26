7304d92ae5d8a82822e4095a9671fc1295167e9d
Efraim Nabil <efraimefefraim@gmail.com>
2023-04-27 00:43:22 +0200

    fixing

--- _printf.c
+++ _printf.c
@@ -8,47 +8,44 @@
  */
 int _printf(const char *fmt, ...)
 {
-    va_list ap;
-    int len = 0, prev_per = 0;
+	va_list ap;
+	int len = 0, prev_per = 0;
 
-    va_start(ap, fmt);
+	va_start(ap, fmt);
 
-    if (fmt == NULL)
-    {
-        return (-1);
-    }
+	if (fmt == NULL)
+		return (-1);
 
-    while (*fmt)
-    {
-        if (prev_per)
-        {
-            if (*fmt != ' ')
-            {
-                len += handle_percent(fmt, ap);
-                prev_per = 0;
-            }
-        }
-        else
-        {
-            if (*fmt == '%')
-            {
-                prev_per = 1;
-            }
-            else
-            {
-                len += write(1, fmt, 1);
-            }
-        }
+	while (*fmt)
+	{
+		if (prev_per)
+		{
+			if (*fmt != ' ')
+			{
+				len += handle_percent(fmt, ap);
+				prev_per = 0;
+			}
+		}
+		else
+		{
+			if (*fmt == '%')
+			{
+				prev_per = 1;
+			}
+			else
+			{
+				len += write(1, fmt, 1);
+			}
+		}
+		fmt++;
+	}
 
-        fmt++;
-    }
+	if (prev_per)
+	{
+		return (-1);
+	}
 
-    if (prev_per)
-    {
-        return (-1);
-    }
+	va_end(ap);
 
-    va_end(ap);
-
-    return (len);
+	return (len);
 }
