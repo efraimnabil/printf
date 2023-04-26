f88d6ebb054d8d4772a6aaabfae63d4f14b4ee37
Efraim Nabil <efraimefefraim@gmail.com>
2023-04-26 02:59:15 +0200

    make it in betty style

--- _printf.c
+++ _printf.c
@@ -6,6 +6,84 @@
  *
  * Return: the number of characters printed
  */
+
+/**
+ * print_string - function that prints a string
+ * @s: the string to print
+ *
+ * Return: the number of characters printed
+ */
+int print_string(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len += write(1, s, 1);
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * print_char - function that prints a character
+ * @c: the character to print
+ *
+ * Return: the number of characters printed
+ */
+int print_char(char c)
+{
+	int len = 0;
+
+	len += write(1, &c, 1);
+	return (len);
+}
+
+/**
+ * just_print - function that prints a character without formatting
+ * @c: the character to print
+ *
+ * Return: the number of characters printed
+ */
+int just_print(char c)
+{
+	int len = 0;
+
+	len += write(1, &c, 1);
+	return (len);
+}
+
+/**
+ * handle_percent - function that handles the "%" character
+ * @fmt: the format string
+ * @ap: the arguments list
+ *
+ * Return: the number of characters printed
+ */
+int handle_percent(char *fmt, va_list ap)
+{
+	int len = 0;
+	char *s;
+	char c;
+
+	fmt++;
+	if (*fmt == 's')
+	{
+		s = va_arg(ap, char *);
+		len += print_string(s);
+	}
+	else if (*fmt == 'c')
+	{
+		c = va_arg(ap, int);
+		len += print_char(c);
+	}
+	else if (*fmt == '%')
+	{
+		len += print_char('%');
+	}
+	return (len);
+}
+
 int _printf(const char *fmt, ...)
 {
 	va_list ap;
@@ -18,25 +96,11 @@ int _printf(const char *fmt, ...)
 	{
 		if (*fmt == '%')
 		{
-			fmt++;
-			switch (*fmt)
-			{
-				case '%':
-					len += write(1, "%", 1);
-					break;
-				case 's':
-					s = va_arg(ap, char *);
-					len += write(1, s, strlen(s));
-					break;
-				case 'c':
-					c = va_arg(ap, int);
-					len += write(1, &c, 1);
-					break;
-			}
+			len += handle_percent((char *)fmt, ap);
 		}
 		else
 		{
-			len += write(1, fmt, 1);
+			len += just_print(*fmt);
 		}
 		fmt++;
 	}
